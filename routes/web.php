a58cdab47a9080c3c3d19d35d2acb30561d8547a
Wanderley <wanderley.efficax@gmail.com>
2018-04-05 10:30:58 -0400

    novo layout

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$this->group(['middleware' => ['auth'] ], function(){
+    Route::get('/home', 'HomeController@index')->name('home');
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,7 +14,7 @@ Route::get('/teste', function () {return special_ucwords('WANDERLEY MACEDO DE PI
 Route::get('/teste2', function () {return verificaTabela('permissions');});
 Route::get('/testecnpj', 'Cadastro\PessoaController@getCnpj')->name('pessoa.cnpj');
 
-Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/user', 'Cadastro\UserController@index')->name('user');
 Route::get('/userpermissions', 'Cadastro\UserController@userpermissions')->name('userpermissions');
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
@@ -43,3 +36,4 @@ Route::get('/pessoa/salvar', ['uses'=>'Cadastro\PessoaController@novaPessoa','as
 Route::post('/telefone/salvar/{id}',['uses'=>'Cadastro\PessoaContato@salvar','as'=>'telefone.salvar']);
 /*End Telefones*/
 
+Route::get('/home', 'HomeController@index')->name('home');
