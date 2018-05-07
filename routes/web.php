c1269c044149933f79fb9ed198830e94d1197a35
Wanderley <wanderley.efficax@gmail.com>
2018-05-07 09:37:03 -0400

    l

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/pessoa/detalhe/{id}', 'Cadastro\PessoaController@detalhe')->name('p
 Route::get('/pessoa/cadastar', 'Cadastro\PessoaController@cadastrar')->name('pessoa.cadastrar');
 Route::get('/pessoa/salvar', 'Cadastro\PessoaController@salvar')->name('pessoa.salvar');
 Route::get('/pessoa/atualizar/{id}', 'Cadastro\PessoaController@atualizar')->name('pessoa.atualizar');
+Route::get('/pessoa/find', 'Cadastro\PessoaController@find');
 
 /*Gupo*/
 Route::get('/pessoagrupo', 'Cadastro\PessoaGrupoController@index')->name('pessoagrupo.index');
@@ -82,40 +83,6 @@ Route::get('naoautorizado', function(){
 });
 
 Route::get('roles-permissions', 'HomeController@rolespermissions')->name('perfil');
-
-<<<<<<< HEAD
-=======
-/*Pessoas*/
-Route::get('/pessoa', 'Cadastro\PessoaController@index')->name('pessoa.index');
-Route::get('/pessoa/detalhe/{id}', 'Cadastro\PessoaController@detalhe')->name('pessoa.detalhe');
-Route::get('/pessoa/cadastar', 'Cadastro\PessoaController@cadastrar')->name('pessoa.cadastrar');
-Route::get('/pessoa/salvar', 'Cadastro\PessoaController@salvar')->name('pessoa.salvar');
-Route::get('/pessoa/atualizar/{id}', 'Cadastro\PessoaController@atualizar')->name('pessoa.atualizar');
-Route::get('/pessoa/find', 'Cadastro\PessoaController@find');
-
-/*Gupo*/
-Route::get('/pessoagrupo', 'Cadastro\PessoaGrupoController@index')->name('pessoagrupo.index');
-Route::get('/pessoagrupo/detalhe/{id}', 'Cadastro\PessoaGrupoController@detalhe')->name('pessoagrupo.detalhe');
-Route::get('/pessoagrupo/cadastrar', 'Cadastro\PessoaGrupoController@cadastrar')->name('pessoagrupo.cadastrar');
-Route::get('/pessoagrupo/salvar', 'Cadastro\PessoaGrupoController@salvar')->name('pessoagrupo.salvar');
-Route::get('/pessoagrupo/atualizar/{id}', 'Cadastro\PessoaGrupoController@atualizar')->name('pessoagrupo.atualizar');
-/*End Grupo*/
-
-/*Contato*/
-Route::get('/pessoa/contato/salvar/{id}', 'Cadastro\PessoaContatoController@salvar')->name('pessoa.contato.salvar');
-Route::get('/pessoa/contato/excluir/{id}', 'Cadastro\PessoaContatoController@excluir')->name('pessoa.contato.excluir');
-Route::get('/pessoa/contato/editar/{id}', 'Cadastro\PessoaContatoController@editar')->name('pessoa.contato.editar');
-Route::get('/pessoa/contato/atualizar/{id}', 'Cadastro\PessoaContatoController@atualizar')->name('pessoa.contato.atualizar');
-/*End Contato*/
-
-/*Endereco*/
-Route::get('/pessoa/endereco/salvar/{id}', 'Cadastro\PessoaEnderecoController@salvar')->name('pessoa.endereco.salvar');
-Route::get('/pessoa/endereco/excluir/{id}', 'Cadastro\PessoaEnderecoController@excluir')->name('pessoa.endereco.excluir');
-Route::get('/pessoa/endereco/editar/{id}', 'Cadastro\PessoaEnderecoController@editar')->name('pessoa.endereco.editar');
-Route::get('/pessoa/endereco/atualizar/{id}', 'Cadastro\PessoaEnderecoController@atualizar')->name('pessoa.endereco.atualizar');
-/*End  Endereco*/
->>>>>>> Pedro
-/*End Pessoas*/
 
 /*Empresas*/
 Route::get('/empresa', 'Cadastro\EmpresaController@index')->name('empresa.index');
